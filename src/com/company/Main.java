58f2a582080743cfc8aa969000756d7d54e06287
Scott Fitzgerald <sfitzgerald@eero.com>
2020-07-07 00:41:05 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        String firstName = "Craig";
+        boolean isCraig = firstName.equals("Craig");
+        firstName.equals("Bob");
+    }
+}
---
+++ src/com/company/PezDispenser.java
@@ -0,0 +1,5 @@
+package com.company;
+
+class PezDispenser {
+    String characterName = "Yoda";
+}
