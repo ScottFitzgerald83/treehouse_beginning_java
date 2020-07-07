a0cb1672e3cffdd17d6d28f21f93217998fe36aa
Scott Fitzgerald <sfitzgerald@eero.com>
2020-07-07 01:04:22 -0500

    implement PezDispenser class and fill dispenser

--- src/com/company/Example.java
+++ src/com/company/Example.java
@@ -1,10 +1,22 @@
 package com.company;
 
 public class Example {
-    public static void main(String[] args) {
-        // Your amazing code goes here
-        System.out.println("We are making a new PEZ Dispenser");
-        PezDispenser dispenser = new PezDispenser();
-        System.out.printf("The dispenser is %s %n", dispenser.characterName);
+
+  public static void main(String[] args) {
+    // Your amazing code goes here...
+    System.out.println("We are making a new PEZ dispenser");
+    System.out.printf("FUN FACT: There are %d PEZ allowed in every dispenser %n", PezDispenser.MAX_PEZ);
+    PezDispenser dispenser = new PezDispenser("Leia");
+    System.out.printf("The dispenser is %s. %n", dispenser.getCharacterName());
+
+    if (dispenser.isEmpty()) {
+      System.out.println("Dispenser is empty.");
+    System.out.println("Filling the dispenser with delicious PEZ...");
+    dispenser.fill();
     }
+
+    if (!dispenser.isEmpty()) {
+      System.out.println("Dispenser is full.");
+    }
+  }
 }
--- src/com/company/PezDispenser.java
+++ src/com/company/PezDispenser.java
@@ -1,5 +1,24 @@
 package com.company;
 
 class PezDispenser {
-    String characterName = "Yoda";
+    public static final int MAX_PEZ = 12;
+    private final String characterName;
+    private int pezCount;
+
+    public PezDispenser(String characterName) {
+        this.characterName = characterName;
+        pezCount = 0;
+    }
+
+    public void fill() {
+        pezCount = MAX_PEZ;
+    }
+
+    public boolean isEmpty() {
+        return pezCount == 0;
+    }
+
+    public String getCharacterName() {
+        return characterName;
+    }
 }
